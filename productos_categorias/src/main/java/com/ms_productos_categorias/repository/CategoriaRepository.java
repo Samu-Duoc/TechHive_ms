3db2a95fb96a3e7eab249c9598cc5106b49f259b
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-11-17 21:35:52 -0300

    Feat: Implementación repositorio para la gestión de categorias en la base de datos

--- productos_categorias/src/main/java/com/ms_productos_categorias/repository/CategoriaRepository.java
+++ productos_categorias/src/main/java/com/ms_productos_categorias/repository/CategoriaRepository.java
@@ -10,4 +10,5 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     boolean existsByNombre(String nombre);
 
     Optional<Categoria> findByNombre(String nombre);
+
 }
